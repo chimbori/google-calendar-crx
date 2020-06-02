ac9e146ef3623e4a11cbe95306dd4f2b43060b49
Manas Tungare <manas@tungare.name>
2020-06-02 16:53:15 -0700

    Fix missing licenses #531

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,5 @@
-// Copyright 2013 Google Inc. All Rights Reserved.
-// Author: manas@google.com (Manas Tungare)
-
+// Copyright 2010 and onwards Google Inc.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
