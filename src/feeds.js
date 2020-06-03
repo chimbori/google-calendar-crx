a753455e7b0a413ce947a44fc6a521f81bf5a16f
Eric Youngson <eayoungs@users.noreply.github.com>
2020-06-03 15:06:06 -0700

    Revert "Modified fetchEventsRecursively_ to allow for a specified rather than relative date range"
    This reverts commit 23f8119216b484a0ff85e2620b761e7087938224.

--- src/feeds.js
+++ src/feeds.js
@@ -340,11 +340,7 @@ feeds.fetchEventsRecursively_ = function(feed, callback, authToken, days, fromDa
     success: (function(feed) {
       return function(data) {
         if (data.items.length == 0) {
-          if (typeof days == "number") {
-            var nextInterval = days + feeds.DAYS_IN_AGENDA_;
-          } else if (typeof days == "text") {
-            var nextInterval = moment().startOf(feeds.DAYS_IN_AGENDA_);
-          }
+          var nextInterval = days + feeds.DAYS_IN_AGENDA_;
           if (nextInterval < feeds.MAX_DAYS_IN_AGENDA_) {
             feeds.fetchEventsRecursively_(feed, callback, authToken, nextInterval, fromDate);
             return;
